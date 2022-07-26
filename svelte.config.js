5bbb8405361907ec5e52b210500e10c4d14a74db
Ambadar <ambadar@micronova.de>
2022-07-26 09:24:10 +0200

    Using adapter auto test

--- svelte.config.js
+++ svelte.config.js
@@ -1,31 +1,18 @@
-import preprocess from 'svelte-preprocess'
-import adapter from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from "svelte-preprocess";
+
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    // default options are shown
-    adapter: vercel({
-      // if true, will deploy the app using edge functions
-      // (https://vercel.com/docs/concepts/functions/edge-functions)
-      // rather than serverless functions
-      edge: false,
+	kit: {
+		adapter: adapter()
+	},
+	preprocess: [
+		preprocess({
+		  postcss: true,
+		}),
+	  ],
+};
 
-      // an array of dependencies that esbuild should treat
-      // as external when bundling functions
-      external: [],
-
-      // if true, will split your app into multiple functions
-      // instead of creating a single one for the entire app
-      split: false
-    })
-  },
-
-  preprocess: [
-    preprocess({
-      postcss: true,
-    }),
-  ],
-}
-
-export default config
+export default config;
